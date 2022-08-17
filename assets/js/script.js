0eeffdfc4cf976742e6bfc10df847f8888a94e14
Ashekboy <ashekun.adeniyi545@gmail.com>
2022-08-18 00:12:55 +0100

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,80 @@
+'use strict';
+
+/**
+ * navbar variables
+ */
+
+const navOpenBtn = document.querySelector("[data-menu-open-btn]");
+const navCloseBtn = document.querySelector("[data-menu-close-btn]");
+const navbar = document.querySelector("[data-navbar]");
+const overlay = document.querySelector("[data-overlay]");
+
+const navElemArr = [navOpenBtn, navCloseBtn, overlay];
+
+for (let i = 0; i < navElemArr.length; i++) {
+
+  navElemArr[i].addEventListener("click", function () {
+
+    navbar.classList.toggle("active");
+    overlay.classList.toggle("active");
+    document.body.classList.toggle("active");
+
+  });
+
+}
+
+
+
+/**
+ * header sticky
+ */
+
+const header = document.querySelector("[data-header]");
+
+window.addEventListener("scroll", function () {
+
+  window.scrollY >= 10 ? header.classList.add("active") : header.classList.remove("active");
+
+});
+
+
+
+/**
+ * go top
+ */
+
+const goTopBtn = document.querySelector("[data-go-top]");
+
+window.addEventListener("scroll", function () {
+
+  window.scrollY >= 500 ? goTopBtn.classList.add("active") : goTopBtn.classList.remove("active");
+
+});
+
+
+function coming() {
+  alert('Alert')
+}
+
+
+function founder() {
+  const w = document.getElementById("founder");
+  var x = document.getElementById("software");
+  var y = document.getElementById("game");
+  var z = document.getElementById("market");
+  
+
+  if(w.style.display !== "none") {
+
+    w.style.display = "none";
+    /*x.style.display = "none";
+    y.style.display = "none";
+    z.style.display = "none";*/
+
+  } else {
+    w.style.display = "block";
+    /*x.style.display = "none";
+    y.style.display = "none";
+    z.style.display = "none";*/
+  }
+}
